53a3d255bfb7770aae63b75cb143d209c7c7da05
Kaushal Singh Thakur <kaushal21gs@gmail.com>
2026-05-20 14:37:44 +0530

    Complete Redis prerequisites

---
+++ src/config/redis.js
@@ -0,0 +1,35 @@
+import { createClient } from "redis";
+
+const redisClient = createClient({
+    url: process.env.REDIS_URL || "redis://localhost:6379"
+});
+
+redisClient.on("connect", () => {
+    console.log("Redis Conntected");
+});
+
+redisClient.on("error", (err) => {
+    console.log("Error", err);
+});
+
+const connectRedis = async () => {
+    try {
+
+        if (!redisClient.isOpen()) {
+            await redisClient.connect();
+        }
+
+    } catch (err) {
+
+        console.log(
+            "Redis Connection Failed:",
+            err
+        )
+
+    }
+};
+
+export {
+    redisClient,
+    connectRedis
+}
