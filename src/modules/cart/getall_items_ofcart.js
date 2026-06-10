63053efd7a1d7ee2bc567462d7a76eccee68f705
Kaushal Singh Thakur <kaushal21gs@gmail.com>
2026-06-10 22:06:36 +0530

    Progress get menu itmes of cart

--- src/modules/cart/getall_items_ofcart.js
+++ src/modules/cart/getall_items_ofcart.js
@@ -1,12 +1,26 @@
 import express from "express";
 
 import db from "../../config/db.js";
+import redisClient from "../../config.js";
 
 const getAllItemsOfCart = express.Router();
 
 getAllItemsOfCart.get("/", async (req, res) => {
 
     const { user_id, role } = req.user;
+
+    const redisCart = await redisClient.get(user_id);
+
+    try {
+
+
+
+    } catch (err) {
+        return res.send({
+            message: "Internal Server Error",
+            error: err.message
+        })
+    }
 
 });
 
